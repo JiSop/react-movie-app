80f99639f2bb31e61759c72ff9948daae1b63a37
JiSop <ddie0821@gmail.com>
2019-12-05 20:42:14 +0900

    Add MovieItem component

---
+++ src/component/MovieItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './MovieItem.scss';
+
+const MovieItem = ({year, title, poster, genres}) => {
+  return (
+    <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie-data">
+          <h3 className="movie-title">{title}</h3>
+          <h5 className="movie-year">{year}</h5>
+          <ul className="movie-genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres-genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+        </div>
+    </div>
+  );
+};
+
+export default MovieItem;
