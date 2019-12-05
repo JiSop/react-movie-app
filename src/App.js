433043f267577ff51986caff58795922afb4d2ae
JiSop <ddie0821@gmail.com>
2019-12-05 20:46:07 +0900

    Fix for MovieList and MovieItem component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+
+import * as api from './lib/api';
+import MovieList from "./component/MovieList";
 
 function App() {
+  const [ movieList, setMovieList ] = useState( null );
+  const [ loading, setLoading ] = useState( false );
+
+  useEffect( () => {
+    const fetchData = async () => {
+      setLoading( true );
+      try {
+        const {
+          data: {
+            data: { movies }
+          }
+        } = await api.getMovieList();
+        setMovieList( movies );
+      } catch (e) {
+        console.log( e );
+      }
+      setLoading( false );
+    };
+    fetchData();
+  }, [] );
+
+  if ( loading ) {
+    return (
+      <div className="loader">
+        <span className="loader-text">Loading...</span>
+      </div>
+    )
+  }
+  if ( !movieList ) {
+    return null;
+  }
   return (
     <div>
+      <section className="container">
+        <MovieList movieList={movieList}/>
+      </section>
     </div>
   );
 }
