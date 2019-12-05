f8fa4cbb5dc8a141835e7cfa87c0599739e7cfa2
JiSop <ddie0821@gmail.com>
2019-12-05 20:41:30 +0900

    Add MovieList  component

---
+++ src/component/MovieList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'MovieList.scss';
+
+import MovieItem from "./MovieItem";
+
+const MovieList = (movieList) => {
+  return (
+    <div className="movie-list">
+      { movieList.map( movie => (
+        <MovieItem
+          key={ movie.id }
+          id={ movie.id }
+          year={ movie.year }
+          title={ movie.title }
+          poster={ movie.medium_cover_image }
+          genres={ movie.genres }
+        />
+      ) ) }
+    </div>
+  );
+};
+
+export default MovieList;
