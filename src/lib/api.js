4af438f8537896ceb7a635b5334281865e500489
JiSop <ddie0821@gmail.com>
2019-12-12 17:41:27 +0900

    Add API request for MovieDetail component

--- src/lib/api.js
+++ src/lib/api.js
@@ -6,9 +6,14 @@ import axios from 'axios';
 // minimum_rating: 	Integer between 0 - 9 (inclusive)
 // order_by: String (desc, asc)
 // genre: String
-export function getMovieList(query) {
-  return axios.get(`https://yts.lt/api/v2/list_movies.json?${query}`);
+export function getMovieList( query ) {
+  return axios.get( `https://yts.lt/api/v2/list_movies.json?${ query }` );
 }
-// export function getMovieList(sort, limit) {
-//   return axios.get(`https://yts.lt/api/v2/list_movies.json?sort=${sort}&limit=${limit}`)
-// }
+
+export function getMovieDetail( id ) {
+  return axios.get( `https://yts.lt/api/v2/movie_details.json?movie_id=${ id }&with_images=true` );
+}
+
+export function getMovieSuggestions( id ) {
+  return axios.get( `https://yts.lt/api/v2/movie_suggestions.json?movie_id=${ id }` );
+}
